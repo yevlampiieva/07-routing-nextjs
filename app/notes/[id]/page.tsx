a370fe6b95ec15d8a87282c97dd8303142f7d6c3
Alla Yevlampiieva <119635848+yevlampiieva@users.noreply.github.com>
2025-09-16 21:33:15 +0300

    Initial commit

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from "next";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
+import NoteDetailsClient from "./NoteDetails.client";
+
+export const metadata: Metadata = {
+  title: "Note detail",
+  description: "Note description",
+};
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function NoteDetails({ params }: Props) {
+  const { id } = await params;
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <NoteDetailsClient />
+      </HydrationBoundary>
+    </div>
+  );
+}
